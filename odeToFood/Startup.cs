0bb3615bcdc3fe24d7f31b315048e533d3615f68
lyang <lyang@westga.edu>
2016-12-15 21:25:43 -0500

    Add project files.

---
+++ odeToFood/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(odeToFood.Startup))]
+namespace odeToFood
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
